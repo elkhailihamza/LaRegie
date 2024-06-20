6f1f903dd44127f83d1c859d00078b05fceafcc5
elkhailihamza <elkhailihamza331@gmail.com>
2024-06-20 11:08:03 +0100

    changed modal submit btn text

--- laRegie/resources/views/components/modal.blade.php
+++ laRegie/resources/views/components/modal.blade.php
@@ -39,7 +39,7 @@
                 </table>
             </div>
             <div class="flex justify-center items-center p-4 md:p-5 border-t border-gray-200 rounded-b">
-                <button type="submit" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Créer</button>
+                <button type="submit" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Complete</button>
             </div>
         </div>
     </div>
